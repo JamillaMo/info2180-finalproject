8ffa2811dd7aff019f807297d21d5dbaee696c29
Carrissa Smith <smithcarrissa1@gmail.com>
2022-12-04 13:40:36 -0800

    View Contact Details Javascript

---
+++ ViewContactDetails.js
@@ -0,0 +1,108 @@
+window.onload= function(){
+
+    var viewlink = document.querySelector('#link');
+    var contactname = document.querySelector('#name');
+    var filterall = document.querySelector(".active filter-all");
+    var filtersales = document.querySelector(".filter-sales");
+    var filtersupport = document.querySelector(".filter-support");
+    var filterassigned = document.querySelector(".filter-assigned");
+
+    viewlink.addEventListener('click', function(element) {
+        element.preventDefault();
+
+        fetch("ViewContact.php?" )
+            .then(response => {
+                if (response.ok){
+                    return response.text()
+                }else {
+                    return Promise.reject('something went wrong')
+                }
+        })
+        .then(data => {
+            console.log;
+        })
+        .catch(error => console.log('There was an error' + error));
+
+    })
+
+    contactname.addEventListener('click', function(element) {
+        element.preventDefault();
+        fetch("ViewContact.php" )
+            .then(response => {
+                if (response.ok){
+                    return response.text()
+                }else {
+                    return Promise.reject('something went wrong')
+                }
+        })
+        .then(data => {
+            console.log;
+        })
+        .catch(error => console.log('There was an error' + error));
+    })
+
+    filterall.addEventListener('click', function(element){
+        element.preventDefault();
+        fetch("dashboard.php?query=filterall" )
+            .then(response => {
+                if (response.ok){
+                    return response.text()
+                }else {
+                    return Promise.reject('something went wrong')
+                }
+        })
+        .then(data => {
+            console.log;
+        })
+        .catch(error => console.log('There was an error' + error));
+    })
+
+    filtersales.addEventListener('click', function(element){
+        element.preventDefault();
+        fetch("dashboard.php?query=filtersales" )
+            .then(response => {
+                if (response.ok){
+                    return response.text()
+                }else {
+                    return Promise.reject('something went wrong')
+                }
+        })
+        .then(data => {
+            console.log;
+        })
+        .catch(error => console.log('There was an error' + error));
+    })
+
+    filtersupport.addEventListener('click', function(element){
+        element.preventDefault();
+        fetch("dashboard.php?query=filtersupport" )
+            .then(response => {
+                if (response.ok){
+                    return response.text()
+                }else {
+                    return Promise.reject('something went wrong')
+                }
+        })
+        .then(data => {
+            console.log;
+        })
+        .catch(error => console.log('There was an error' + error));
+    })
+
+    filterassigned.addEventListener('click', function(element){
+        element.preventDefault();
+        fetch("dashboard.php?query=filterassigned" )
+            .then(response => {
+                if (response.ok){
+                    return response.text()
+                }else {
+                    return Promise.reject('something went wrong')
+                }
+        })
+        .then(data => {
+            console.log;
+        })
+        .catch(error => console.log('There was an error' + error));
+    })
+        
+}
